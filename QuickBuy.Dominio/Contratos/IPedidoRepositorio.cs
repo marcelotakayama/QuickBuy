81608caf0c1f97c60bc2faf3751c0ada15ad1bbe
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-07 14:58:04 -0300

    Especificando os contratos do repositório

---
+++ QuickBuy.Dominio/Contratos/IPedidoRepositorio.cs
@@ -0,0 +1,9 @@
+﻿using QuickBuy.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.Contratos {
+    public interface IPedidoRepositorio : IBaseRepositorio<Pedido> {
+    }
+}
