2689bc3ca7a852f8aae5b3569bb1ebd159132c8d
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-07 12:30:18 -0300

    deixando a classe FormaPagamento pública para corrigir bug

--- QuickBuy.Dominio/ObjetoDeValor/FormaPagamento.cs
+++ QuickBuy.Dominio/ObjetoDeValor/FormaPagamento.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace QuickBuy.Dominio.ObjetoDeValor {
-    class FormaPagamento {
+    public class FormaPagamento {
         public int Id { get; set; }
         public string Nome { get; set; }
         public string Descricao { get; set; }
