eff5a48eba7321777d0239b3a92b3ea08dc4dabd
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-07 17:22:27 -0300

    Teste

--- QuickBuy.Web/Startup.cs
+++ QuickBuy.Web/Startup.cs
@@ -52,6 +52,7 @@ namespace QuickBuy.Web {
             });
 
             app.UseSpa(spa => {
+                //teste
                 // To learn more about options for serving an Angular SPA from ASP.NET Core,
                 // see https://go.microsoft.com/fwlink/?linkid=864501
 
