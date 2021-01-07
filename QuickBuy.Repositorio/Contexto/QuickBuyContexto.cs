a7000e7e22adf63f6f460781cb8560f5a2082363
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-07 17:06:27 -0300

    Configurando contexto do banco de dados

--- QuickBuy.Repositorio/Contexto/QuickBuyContexto.cs
+++ QuickBuy.Repositorio/Contexto/QuickBuyContexto.cs
@@ -13,5 +13,8 @@ namespace QuickBuy.Repositorio.Contexto {
         public DbSet<Pedido> Pedidos { get; set; }
         public DbSet<ItemPedido> ItensPedidos { get; set; }
         public DbSet<FormaPagamento> FormaPagamento { get; set; }
+
+        public QuickBuyContexto(DbContextOptions options) : base(options) {
+        }
     }
 }
