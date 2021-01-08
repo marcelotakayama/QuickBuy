edbd31cb142ca52f79a557247231efa3b32387a6
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-08 10:36:59 -0300

    Mapeando 1 pra muitos por convenção

--- QuickBuy.Web/Startup.cs
+++ QuickBuy.Web/Startup.cs
@@ -23,7 +23,9 @@ namespace QuickBuy.Web {
         public void ConfigureServices(IServiceCollection services) {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             var connectionString = Configuration.GetConnectionString("mysqlconnection");
-            services.AddDbContext<QuickBuyContexto>(options => options.UseMySql(connectionString, m=> m.MigrationsAssembly("QuickBuy.Repositorio")));
+            services.AddDbContext<QuickBuyContexto>(option =>
+                                                        option.UseLazyLoadingProxies()
+                                                        .UseMySql(connectionString, m => m.MigrationsAssembly("QuickBuy.Repositorio"))) ;
 
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration => {
