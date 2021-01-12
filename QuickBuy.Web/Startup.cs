971222d57d9e02ac4e22ab98d7ba894859fb53dd
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-12 17:13:54 -0300

    Subindo algumas coisas porém o banco ainda não é reconhecido dentro do mysql workbench, precisa revisar toda a parte da string de conexão e tentar subir em outra instância do banco de dados local

--- QuickBuy.Web/Startup.cs
+++ QuickBuy.Web/Startup.cs
@@ -22,10 +22,8 @@ namespace QuickBuy.Web {
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-            var connectionString = Configuration.GetConnectionString("mysqlconnection");
-            services.AddDbContext<QuickBuyContexto>(option =>
-                                                        option.UseLazyLoadingProxies()
-                                                        .UseMySql(connectionString, m => m.MigrationsAssembly("QuickBuy.Repositorio"))) ;
+            var connectionString = Configuration.GetConnectionString("QuickBuyDB");
+            services.AddDbContext<QuickBuyContexto>(option => option.UseMySql(connectionString, m => m.MigrationsAssembly("QuickBuy.Repositorio"))) ;
 
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration => {
